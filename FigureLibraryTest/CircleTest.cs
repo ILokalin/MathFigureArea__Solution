923cb01d47b6dc78b2f103bbf0afc0bcc360d05b
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-02 01:06:44 +0500

    build(solution): create solution for FigureLibrary and test app

---
+++ FigureLibraryTest/CircleTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FigureLibraryTest
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
