1a4405dbabe39309bd07c40b4d623436e0806089
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-02 07:29:49 +0500

    feat(Triangle): create validate and tests

--- FigureLibrary/Triangle.cs
+++ FigureLibrary/Triangle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace FigureLibrary
 {
@@ -33,7 +34,15 @@ namespace FigureLibrary
         /// <param name="radius">значение радиуса double[]</param>
         public Triangle(double[] sides) 
         {
+            if (TriangleValidate(sidesToList(sides)))
+            {
                 Type = "triangle";
+            }
+        }
+
+        public static double getArea(double[] sides)
+        {
+            return 0;
         }
 
         /// <summary>
@@ -45,6 +54,57 @@ namespace FigureLibrary
             return Type;
         }
 
+        private static List<double> sidesToList(double[] sides)
+        {
+            List<double> sidesList = new List<double>();
+            foreach(var side in sides)
+            {
+                sidesList.Add(side);
+            }
 
+            return sidesList;
+        }
+
+        /// <summary>
+        /// Валидация треугольника. Фигура соответствует треугольнику если:
+        /// - нет сторон больше суммы двух других
+        /// - нет сторон с отрицательным значением
+        /// - сумма всех сторон равна 0 (создание пустого объкта)
+        /// - заданы значения для трех сторон
+        /// </summary>
+        /// <param name="radius">значение радиуса double</param>
+        /// <returns>true - треугольник; исключение - при несоответствии</returns>
+        private static bool TriangleValidate(List<double> sides)
+        {
+            double sumOfSides = sides.Sum();
+            sides.Sort();
+
+            if (sides.Count < 3)
+            {
+                throw new NotValidateException("Too few parameters for sides");
+            }
+
+            if (sides.Count > 3)
+            {
+                throw new NotValidateException("Too many parameters for sides"); 
+            }
+
+            sides.ForEach(delegate (double side)
+            {
+                if (side <= 0 & sumOfSides != 0)
+                {
+                    throw new NotValidateException("One of the side has bad size (0 or negative)");
+                }
+            });
+
+            if (sides[2] > sides[1] + sides[0] & sumOfSides != 0)
+            {
+                throw new NotValidateException("One of the sides too long");
+            }
+
+            
+
+            return true;
+        }
     }
 }
--- FigureLibraryTest/TriangleTest.cs
+++ FigureLibraryTest/TriangleTest.cs
@@ -20,5 +20,77 @@ namespace FigureLibraryTest
             Triangle testCircleWithArray = new Triangle(new double[] { 5, 5, 5 });
             Assert.AreEqual(testCircleWithArray.ToString(), "triangle");
         }
+
+        [TestMethod]
+        public void triangleException()
+        {
+            try
+            {
+                Triangle testCircleNotACircle = new Triangle(5, 15, 55);
+                Assert.Fail("An exception should have been throw");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "One of the sides too long");
+            }
+
+            try
+            {
+                Triangle testCircleNotACircle = new Triangle(new double[] { 3, 4});
+                Assert.Fail("An exception should have been throw");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Too few parameters for sides");
+            }
+
+            try
+            {
+                Triangle testCircleNotACircle = new Triangle(new double[] { 3, 4, 7, 3 });
+                Assert.Fail("An exception should have been throw");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Too many parameters for sides");
+            }
+
+            try
+            {
+                Triangle testCircleNotACircle = new Triangle(new double[] { 0, 4, 4 });
+                Assert.Fail("An exception should have been throw");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "One of the side has bad size (0 or negative)");
+            }
+
+            try
+            {
+                Triangle testCircleNotACircle = new Triangle(new double[] { -5, 4, 4 });
+                Assert.Fail("An exception should have been throw");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "One of the side has bad size (0 or negative)");
+            }
+        }
+
+        [TestMethod]
+        public void TriangleGetArea()
+        {
+            List<SFigureTest> triangleList = new List<SFigureTest>();
+
+            triangleList.Add(new SFigureTest() { Sides = new double[] { 13, 5, 14 }, CheckArea = 32.496154 });
+            triangleList.Add(new SFigureTest() { Sides = new double[] { 5, 5, 5 }, CheckArea = 10.825318 });
+            triangleList.Add(new SFigureTest() { Sides = new double[] { 7, 3, 9 }, CheckArea = 8.785642 });
+
+            triangleList.ForEach(delegate (SFigureTest triangleTest)
+            {
+                var (sides, checkArea, checkName, checkWeight) = triangleTest;
+                double result = Triangle.getArea(sides);
+                Assert.AreEqual(result, checkArea, 0.0001, String.Format("Triangle with sides '{0}, {1}, {2}' ", sides[0], sides[1], sides[2]));
+            });
+        }
+
     }
 }
