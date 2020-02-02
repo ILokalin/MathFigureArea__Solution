f482efd07a53f2606bfeff9b558333d8d7039bad
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-02 06:26:02 +0500

    feat(Triangle): create constructor and test override

--- FigureLibrary/Triangle.cs
+++ FigureLibrary/Triangle.cs
@@ -6,5 +6,45 @@ namespace FigureLibrary
 {
     public class Triangle
     {
+        /// <summary>
+        /// Тип фигуры
+        /// </summary>
+        public string Type { get; }
+
+        /// <summary>
+        /// Площадь треугольника
+        /// </summary>
+        public double Area { get; }
+
+        /// <summary>
+        /// Конструктор Triangle
+        /// </summary>
+        public Triangle() : this(0, 0, 0) { }
+
+        /// <summary>
+        /// Конструктор Triangle
+        /// </summary>
+        /// <param name="radius">значение радиуса double</param>
+        public Triangle(double sideA, double sideB, double sideC) : this(new double[] { sideA, sideB, sideC }) { }
+
+        /// <summary>
+        /// Конструктор Triangle
+        /// </summary>
+        /// <param name="radius">значение радиуса double[]</param>
+        public Triangle(double[] sides) 
+        {
+                Type = "triangle";
+        }
+
+        /// <summary>
+        /// Получить название фигуры
+        /// </summary>
+        /// <returns>название фигуры string</returns>
+        public override string ToString()
+        {
+            return Type;
+        }
+
+
     }
 }
--- FigureLibraryTest/TriangleTest.cs
+++ FigureLibraryTest/TriangleTest.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FigureLibrary;
 
 namespace FigureLibraryTest
 {
-    class TriangleTest
+    [TestClass]
+    public class TriangleTest
     {
+        [TestMethod]
+        public void circleConstructor()
+        {
+            Triangle testCircle = new Triangle();
+            Assert.AreEqual(testCircle.ToString(), "triangle");
+
+            Triangle testCircleWithValue = new Triangle(5, 5, 5);
+            Assert.AreEqual(testCircleWithValue.ToString(), "triangle");
+
+            Triangle testCircleWithArray = new Triangle(new double[] { 5, 5, 5 });
+            Assert.AreEqual(testCircleWithArray.ToString(), "triangle");
+        }
     }
 }
