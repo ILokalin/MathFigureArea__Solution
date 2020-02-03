cf16f5fa7239a25e52888ee07d1a795c5065250e
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-03 06:42:02 +0500

    feat(Service): create service class for refactoring. Sort for Array

---
+++ FigureLibrary/Service.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FigureLibrary
+{
+    public class Service
+    {
+        private static void Swap(double[] swapArray, int indexA, int indexB)
+        {
+            double tempSwap = swapArray[indexA];
+            swapArray[indexA] = swapArray[indexB];
+            swapArray[indexB] = tempSwap;
+        }
+
+        public static void arraySort(double[] sortArray)
+        {
+            int left = 0;
+            int right = sortArray.Length - 1;
+
+            while (left < right)
+            {
+                for (int i = left; i < right; i++ )
+                {
+                    if (sortArray[i] > sortArray[i + 1])
+                    {
+                        Swap(sortArray, i, i + 1);
+                    }
+                }
+                right--;
+
+                for (int i = right; i > left; i--)
+                {
+                    if (sortArray[i-1] > sortArray[i])
+                    {
+                        Swap(sortArray, i, i - 1);
+                    }
+                }
+                left++;
+            }
+        }
+    }
+}
---
+++ FigureLibraryTest/SeriveseTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FigureLibrary;
+using System.Linq;
+
+namespace FigureLibraryTest
+{
+    [TestClass]
+    public class SeriveseTest
+    {
+        struct SDoubleArray
+        {
+            public double[] CheckArray;
+            public double[] ResultArray;
+        }
+
+        [TestMethod]
+        public void testSortServise()
+        {
+            List<SDoubleArray> testArrays = new List<SDoubleArray>();
+
+            testArrays.Add(new SDoubleArray() { CheckArray = new double[] { 45, 12, 4.8, 38.89783, 74 }, ResultArray = new double[] { 4.8, 12, 38.89783, 45, 74 } });
+            testArrays.Add(new SDoubleArray() { CheckArray = new double[] { 2, 36.4354, 5, 17, 0 }, ResultArray = new double[] { 0, 2, 5, 17, 36.4354 } });
+            //testArrays.Add(new SDoubleArray() { CheckArray = new double[] { 45, 12 }, ResultArray = new double[] { 12, 45 } });
+            //testArrays.Add(new SDoubleArray() { CheckArray = new double[] { 45 }, ResultArray = new double[] { 45 } });
+            testArrays.Add(new SDoubleArray() { CheckArray = new double[] { 45, 12, 4.8 }, ResultArray = new double[] { 4.8, 12, 45 } });
+
+            testArrays.ForEach(delegate (SDoubleArray CompareArrays)
+            {
+                Service.arraySort(CompareArrays.CheckArray);
+                IStructuralEquatable ComparePartCheck =CompareArrays.CheckArray;
+                bool result = ComparePartCheck.Equals(CompareArrays.ResultArray, StructuralComparisons.StructuralEqualityComparer);
+
+                string stringArray = "";
+
+                foreach (var item in CompareArrays.CheckArray)
+                {
+                    stringArray += item.ToString() + " ";
+                }
+
+                Assert.AreEqual(result, true, String.Format("Sort array {0} fail", stringArray));
+            });
+        }
+    }
+}
