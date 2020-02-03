d377a6ffd6e0820353ec9bfdf81bf99ade0dd8e2
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-03 09:09:00 +0500

    build(DemoApp): create console application for demo

---
+++ DemoApp/Demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
